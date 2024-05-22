79ada8d8649f3e8e5ca8489b3980c6768e6baf36
aradmschool <arad.n@masterschool.com>
2024-05-22 16:23:03 -0400

    refactor web scraper

--- scripts/soupify_kaggle.py
+++ scripts/soupify_kaggle.py
@@ -3,26 +3,38 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-dataset_url = sys.argv[1] # this let's us give the script a url as input from the command line
+def get_dataset_description(dataset_url):
 
-response = requests.get(dataset_url)
+    """ scrapes kaggle page for dataset description and name
+        input: string (dataset_url)
+        output: dict {name: .., description: ..} 
+    """
 
-if response.status_code == 200:
-    print("it worked")
-    soup = BeautifulSoup(response.text,'html.parser')
-else:
-    print("didn't work")
-    print(f"response status: {response.status_code}")
+    description_d = {"name":"not found","description":"not found"}
 
-script_tag = soup.find('script', {'type': 'application/ld+json'})
+    response = requests.get(dataset_url)
 
-if script_tag:
-    json_data = json.loads(script_tag.string)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text,'html.parser')
+    else:
+        print("didn't work!\n")
+        print(f"response status: {response.status_code}")
 
-    description = json_data.get('description', '')
-    name = json_data.get('name', '')
+    script_tag = soup.find('script', {'type': 'application/ld+json'})
 
-    print(description)
-    print(name)
-else:
-    print("Script tag not found.")
+    if script_tag:
+        json_data = json.loads(script_tag.string)
+
+        description_d["name"] = json_data.get('name', '')
+        description_d["description"] = json_data.get('description', '')
+    else:
+        print("script tag not found.\nreturning empty strings")
+
+    return description_d
+
+if __name__ == "__main__":
+    dataset_url = sys.argv[1] # this let's us give the script a url as input from the command line
+    dataset_name = dataset_url.rsplit("/",1)[-1]
+    description_d = get_dataset_description(dataset_url)
+    with open(f"{dataset_name}.json","w") as j:
+        json.dump(description_d,j)
