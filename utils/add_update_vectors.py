9d4bf156cca15649996bbe3b612b18ecbf0eae8e
aradmschool <arad.n@masterschool.com>
2024-05-31 11:56:37 -0400

    change path

--- utils/add_update_vectors.py
+++ utils/add_update_vectors.py
@@ -3,14 +3,14 @@ import pandas as pd
 import chromadb
 from time import sleep
 
-df = pd.read_csv('/Users/arad/repos/pp_kaggle_query/csvs/kaggle_datasets_for_chroma.csv')
+df = pd.read_csv('../csvs/kaggle_datasets_for_chroma.csv')
 df.sort_values('TotalDownloads',ascending=False)
 df = df.head(30_000)
 
 # (to avoid rate limits)
 subsets = [df[i:i+1500] for i in range(0, df.shape[0], 1500)]
 
-client = chromadb.PersistentClient(path="/Users/arad/repos/pp_kaggle_query/db")
+client = chromadb.PersistentClient(path="../db")
 
 collection = client.get_or_create_collection("kaggle",
                                              embedding_function=openai_ef,
